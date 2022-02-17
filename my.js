cb0ede90025e06d6ad27cfb33acff794d4a797fe
user00777777 <97362584+user00777777@users.noreply.github.com>
2022-02-17 23:16:54 +0200

    е
    е

---
+++ my.js
@@ -0,0 +1,11 @@
+var c
+function myage(  r     ){
+if( 1984 >=r){alert(  "   старушка")  }
+if(r>=1990){alert("более менее")}
+ if( "юля"==r  ){alert( "Ты самая самая лучшая")   }
+
+}
+
+
+var w=prompt();
+myage(w)
